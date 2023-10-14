21bad0d1b5421480037d6cabe386902561162dcd
visual studio <vs@itstep.ge>
2023-10-14 19:50:30 +0400

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,30 @@
+﻿static string Encrypt(string input, int key)
+{
+    string encrypted = string.Empty;
+        for(int i = 0; i < input.Length; i++) {
+
+        if (input[i] == ' ')
+            encrypted += " ";
+        else
+            encrypted += (char)((int)input[i] + key);
+    }
+        return encrypted;
+}
+
+
+Console.WriteLine(Encrypt("abc cde", 2));
+
+static string Decrypt(string input, int key)
+{
+    string decrypted = string.Empty;
+    for(int i = 0; i < input.Length; i++){
+        if(input[i] == ' ')
+            decrypted += " ";
+           else 
+                decrypted += (char)((int)input[i] - key);
+        }
+    return decrypted;
+}
+string enc = Encrypt("abc cde", 2);
+
+Console.WriteLine(Decrypt(enc,2));
